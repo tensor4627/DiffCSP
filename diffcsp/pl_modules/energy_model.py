9a49ee5a9e5f8590b005aedf9326d81e205c3ed3
tensor4627 <josephjostar609@gmail.com>
2025-12-31 15:16:27 +0800

    force-fix

--- diffcsp/pl_modules/energy_model.py
+++ diffcsp/pl_modules/energy_model.py
@@ -558,7 +558,7 @@ class CSPEnergyMatching(BaseModule):
                 grad_f, grad_l = grad(pred_e, [input_frac_coords,input_lattice], grad_outputs = grad_outputs,create_graph=True,allow_unused=True)
         loss_lattice = F.mse_loss(torch.tril(-grad_l), torch.tril(lattices-rand_l))
         grad_x = grad_f.view(-1,1,3)@input_lattice.transpose(-1,-2).repeat_interleave(batch.num_atoms,dim=0)
-        loss_coord = F.mse_loss(-torch.bmm(-grad_x), self.wrapped_distance_vector(rand_x,frac_coords))
+        loss_coord = F.mse_loss((-grad_x), self.wrapped_distance_vector(rand_x,frac_coords))
 
         loss = (
             self.hparams.cost_lattice * loss_lattice +
